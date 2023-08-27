abdfd8279d458f2afa10f5fb13db0fe7a8a7c874
Xuan MA <xuan.ma.au@gmail.com>
2023-08-27 17:34:29 +1000

    Add unit tests for AppUserDetails

--- src/test/java/org/mmx/comment/AppTestBase.java
+++ src/test/java/org/mmx/comment/AppTestBase.java
@@ -3,6 +3,9 @@ package org.mmx.comment;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+/**
+ * Base class for unit testing
+ */
 @ExtendWith(MockitoExtension.class)
 public class AppTestBase {
 }
